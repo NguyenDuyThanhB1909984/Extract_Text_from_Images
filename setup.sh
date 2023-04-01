752af96ec44d41f3fed43aa6d6d10468a2074a97
NguyenDuyThanhB1909984 <101155685+NguyenDuyThanhB1909984@users.noreply.github.com>
2023-04-01 21:19:08 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,3 @@
+# Download Vietnamese language data for Tesseract OCR
+mkdir -p /usr/share/tesseract-ocr/4.00/tessdata/
+wget -O /usr/share/tesseract-ocr/4.00/tessdata/vie.traineddata https://github.com/tesseract-ocr/tessdata/raw/main/vie.traineddata
