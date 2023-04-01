f1b74576e1d6a41586e87918d8886c8e457ef67d
NguyenDuyThanhB1909984 <101155685+NguyenDuyThanhB1909984@users.noreply.github.com>
2023-04-01 21:46:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,19 +16,38 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 st.title("Trích xuất văn bản từ hình ảnh")
 
-# Upload image file
-uploaded_file = st.file_uploader("Chọn hình ảnh...", type=["jpg", "jpeg", "png"])
+# # Upload image file
+# uploaded_file = st.file_uploader("Chọn hình ảnh...", type=["jpg", "jpeg", "png"])
 
-# Extract text from uploaded image
-if uploaded_file is not None:
-    # Display uploaded image
-    image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
-    st.image(image, caption='Hình ảnh đã tải lên', use_column_width=True)
+# # Extract text from uploaded image
+# if uploaded_file is not None:
+#     # Display uploaded image
+#     image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+#     st.image(image, caption='Hình ảnh đã tải lên', use_column_width=True)
 
-    # Extract text from image
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    text = pytesseract.image_to_string(gray, lang='vie')
+#     # Extract text from image
+#     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#     text = pytesseract.image_to_string(gray, lang='vie')
 
-    # Display extracted text
-    st.subheader('Văn bản trích xuất')
-    st.write(text)
+#     # Display extracted text
+#     st.subheader('Văn bản trích xuất')
+#     st.write(text)
+
+# Upload image files
+uploaded_files = st.file_uploader("Chọn hình ảnh...", type=["jpg", "jpeg", "png"], accept_multiple_files=True)
+
+# Extract text from uploaded images
+if uploaded_files is not None:
+    for uploaded_file in uploaded_files:
+        # Display uploaded image
+        image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
+        st.image(image, caption='Uploaded Image', use_column_width=True)
+
+        # Extract text from image
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        text = pytesseract.image_to_string(gray, lang='vie')
+
+        # Display extracted text
+        st.subheader('Văn bản trích xuất')
+        st.write(text)
+
